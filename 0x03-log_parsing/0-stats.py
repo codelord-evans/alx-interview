0c0b4c936e991cf9b3dc3c6d7a2a5595a34e55cc
codelord-evans <ekioko404@gmail.com>
2023-10-19 04:13:15 -0700

    correction

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This is a script that reads stdin line by line and computes metrics:
+""" write a script that reads stdin line by line and computes metrics:
 
 Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
 <status code> <file size> (if the format is not this one, the line
